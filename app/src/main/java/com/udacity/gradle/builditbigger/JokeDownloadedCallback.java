a9900c448dd361062b09851c4ca5a525ffdc7d41
Javier Arboleda <javier.g.arboleda@gmail.com>
2016-01-18 20:22:04 -0800

    Added JokeDownloadedCallback.java

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeDownloadedCallback.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by javierarboleda on 1/15/16.
+ */
+public interface JokeDownloadedCallback {
+
+    void jokeDownloaded(String joke);
+}
